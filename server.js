df243a03a0d07c007f42aebf2fd17b280ee1199b
Austin Styer <austin.j.styer@gmail.com>
2024-10-02 20:22:43 -0600

    working on

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get, ("/",baseController.buildHome)
-// app.get("/", function(req, res){
-//   res.render("index", {title: "Home"})
-// })
+app.get, ("/", baseController.buildHome)
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
